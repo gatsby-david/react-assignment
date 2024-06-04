346b2e8a2794afb6bf0a96ed07d866573c9a4c7a
David Holada <david.holada@synetech.cz>
2024-06-04 13:32:13 +0200

    F2: Load todos from API

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,12 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import { List } from "./List";
 import { ListItem } from "./ListItem";
+import axios from "axios";
+
+export type Todo = {
+    label: string;
+    isDone: boolean;
+    createdAt: number;
+    id: number;
+};
 
 export const Todos = () => {
+    const { isPending, error, data } = useQuery({
+        queryKey: ["todos"],
+        queryFn: () => axios.get("http://localhost:3000/items").then((res) => res.data),
+    });
+
+    if (isPending) return "Loading...";
+
+    if (error) return "An error has occurred: " + error.message;
+
     return (
         <List>
-            <ListItem label="item" isDone={true} />
-            <ListItem label="item" isDone={true} />
-            <ListItem label="item" isDone={true} />
+            {data.map((todo: Todo) => (
+                <ListItem label={todo.label} isDone={todo.isDone} key={todo.createdAt} />
+            ))}
         </List>
     );
 };
