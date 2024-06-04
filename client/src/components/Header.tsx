825ba39038ff828b492960f83923f37b97706dfd
David Holada <david.holada@synetech.cz>
2024-06-04 16:14:37 +0200

    F3: Add new todo feature

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { PlusIcon } from "@radix-ui/react-icons";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Form } from "./form";
+import { ErrorMessage } from "./form/ErrorMessage";
 
 const StyledDiv = styled.header`
     display: flex;
@@ -15,25 +17,47 @@ const StyledDiv = styled.header`
         border: 1px solid;
         border-color: ${(props) => props.theme.colors.olive9};
         border-radius: 50%;
+        text-align: center;
 
         color: #fff;
     }
 `;
 
+const FormWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
 type HeaderProps = {
     children: React.ReactNode;
     onItemAdd: (label: string) => void;
+    isAPIerror: boolean;
 };
 
 export const Header = (props: HeaderProps) => {
-    const { children } = props;
+    const { children, onItemAdd, isAPIerror } = props;
+    const [isFormOpen, setIsFormOpen] = useState<boolean>(false);
 
     return (
         <StyledDiv>
             <h1>{children}</h1>
-            <button>
+            <button
+                onClick={() => {
+                    setIsFormOpen(true);
+                }}
+            >
                 <PlusIcon />
             </button>
+            <FormWrapper>
+                {isFormOpen && (
+                    <Form
+                        onCancel={() => setIsFormOpen(false)}
+                        onSubmit={(value) => onItemAdd(value)}
+                        initialValue="New todo"
+                    />
+                )}
+                {isAPIerror && <ErrorMessage>Error occured, please try again.</ErrorMessage>}
+            </FormWrapper>
         </StyledDiv>
     );
 };
--- client/src/components/HeaderWrapper.tsx
+++ client/src/components/HeaderWrapper.tsx
@@ -1,5 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Header } from "./Header";
+import { Todo } from "./Todos";
+import axios from "axios";
+import { useState } from "react";
 
 export const HeaderWrapper = () => {
-    return <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>;
+    const queryClient = useQueryClient();
+
+    const [error, setError] = useState<boolean>(false);
+
+    const mutation = useMutation({
+        mutationFn: (newTodo: Omit<Todo, "id" | "createdAt">) => {
+            return axios.post("http://localhost:3000/items", newTodo);
+        },
+        onError: () => {
+            setError(true);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos"] });
+            setError(false);
+        },
+    });
+
+    const addItem = (label: string) => {
+        mutation.mutate({ label, isDone: false });
+        queryClient.invalidateQueries({ queryKey: ["todos"] });
+    };
+
+    return (
+        <Header onItemAdd={addItem} isAPIerror={error}>
+            To Do app
+        </Header>
+    );
 };
