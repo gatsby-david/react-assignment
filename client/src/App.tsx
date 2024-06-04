5cdf23bbabdeb9b142d1f1522682fa8d2e52f806
David Holada <david.holada@synetech.cz>
2024-06-04 13:25:31 +0200

    F2: Add react-query provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,28 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { ThemeProvider } from "./components/providers/ThemeProvider";
 import { ListItem } from "./components/ListItem";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const App = () => (
-    <ThemeProvider>
-        <Container>
-            <Layout>
-                <div>
-                    <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
-                    <List>
-                        <ListItem label="item" isDone={true} />
-                        <ListItem label="item" isDone={true} />
-                        <ListItem label="item" isDone={true} />
-                    </List>
-                </div>
-                <Footer />
-            </Layout>
-        </Container>
-    </ThemeProvider>
-);
+export const App = () => {
+    const queryClient = new QueryClient();
+
+    return (
+        <ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <Container>
+                    <Layout>
+                        <div>
+                            <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
+                            <List>
+                                <ListItem label="item" isDone={true} />
+                                <ListItem label="item" isDone={true} />
+                                <ListItem label="item" isDone={true} />
+                            </List>
+                        </div>
+                        <Footer />
+                    </Layout>
+                </Container>
+            </QueryClientProvider>
+        </ThemeProvider>
+    );
+};
